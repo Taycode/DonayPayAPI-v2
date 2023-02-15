7cbaae2248803ebf23359e3805858466eb7d83f0
Abdulmateen Tairu <tay2druh@gmail.com>
2023-02-15 10:57:50 +0100

    some eslint fix

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 export default class App {
   static async boot() {
     const app = express();
+    app.use(cors());
+    app.use(bodyParser.json());
     return app;
   }
 }

13bf033b581120948616f3901de7c0474fc2a24a
Abdulmateen Tairu <tay2druh@gmail.com>
2023-02-15 12:10:14 +0100

    swagger docs setup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import AppRoute from './routes';
+import Docs from './docs';
 
 export default class App {
   static async boot() {
@@ -9,6 +10,7 @@ export default class App {
     app.use(cors());
     app.use(bodyParser.json());
     AppRoute(app);
+    Docs(app);
     return app;
   }
 }
